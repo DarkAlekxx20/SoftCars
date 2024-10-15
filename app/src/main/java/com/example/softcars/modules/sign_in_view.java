d23c60973e19e1104bfb045a2cd65c7e7ac91890
DarkAlekxx20 <niko.beelik@hotmail.com>
2024-10-14 20:13:02 -0600

    Se genera archivos para vista de creacion de usuarios para la aplicacion

---
+++ app/src/main/java/com/example/softcars/modules/sign_in_view.java
@@ -0,0 +1,26 @@
+package com.example.softcars.modules;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.softcars.R;
+
+public class sign_in_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_in_view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
